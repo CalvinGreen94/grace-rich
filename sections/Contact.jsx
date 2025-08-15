bd47d084d418ee26056addd4ded9f5d5e0a56e38
Calvin Green <calvingreen94@aol.com>
2025-08-14 20:04:39 -0400

    grace-rich

--- sections/Contact.jsx
+++ sections/Contact.jsx
@@ -1,21 +1,21 @@
 import { InlineWidget } from 'react-calendly';
-import { Helmet } from 'react-helmet-async';
+import Head from 'next/head';
 
 export default function Contact() {
   return (
     <>
-      <Helmet>
-        <title>DeFit | Book a Session</title>
+      <Head>
+        <title>Perfect Fit | Book a Session</title>
         <meta
           name="description"
-          content="Schedule a personal training session with Grace through Calendly."
+          content="Schedule a personalized clothing alteration session with Grace using Calendly. Select a convenient time to ensure your garments fit perfectly and look their best."
         />
-      </Helmet>
+      </Head>
 
       <section data-scroll-section>
         <h2>Book a Session</h2>
         <div style={{ maxWidth: '600px', margin: '0 auto' }}>
-          <InlineWidget url='https://calendly.com/lafrancdai/alterations-brainstorming' />
+        <InlineWidget url='https://calendly.com/lafrancdai/alterations-brainstorming' />
         </div>
       </section>
     </>
